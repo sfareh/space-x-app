ee234da1c8ce627cd6abe5ba356b3cddecb26441
Sarah <sarouini@gmail.com>
2022-03-02 12:20:07 +0100

    new component capsules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LaunchesComponent } from './launches/launches.component';
+import { CapsulesComponent } from './capsules/capsules.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LaunchesComponent
+    LaunchesComponent,
+    CapsulesComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
