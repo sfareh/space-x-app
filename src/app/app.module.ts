e83ed3c28fb74f3171db7fa0407bb807dd05d148
Sarah <sarouini@gmail.com>
2022-03-02 11:56:32 +0100

    new component launches

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LaunchesComponent } from './launches/launches.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LaunchesComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
