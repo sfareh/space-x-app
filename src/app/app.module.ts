7923447f3f57e8c11ebedff4ba779d9187eb6958
Mohamed Desoki <m.desoki@hotmail.fr>
2022-03-03 10:14:49 +0100

    update capsules views and ts config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LaunchesComponent } from './launches/launches.component';
 import { CapsulesComponent } from './capsules/capsules.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CapsulesComponent } from './capsules/capsules.component';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
