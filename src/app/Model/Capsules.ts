1a7d24bd58fe900808af63ceedd67d2fa956ebfe
Mohamed Desoki <m.desoki@hotmail.fr>
2022-03-03 10:06:27 +0100

    added capsules model

---
+++ src/app/Model/Capsules.ts
@@ -0,0 +1,12 @@
+export class Capsules {
+
+  id:string;
+  type: string;
+  status: string;
+  launches: string;
+  last_update: string;
+  land_landings:number;
+  water_landings:number;
+  reuse_count:number;
+
+}
