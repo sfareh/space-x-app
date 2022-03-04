2b66aec9e47900a9eb37ca859ca2870fdf75e7aa
RichardMargin <richard.mangin@mail.com>
2022-03-04 19:38:37 +0100

    added method to get launches from rocket id

--- src/app/Services/app.service.ts
+++ src/app/Services/app.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Launches } from '../Model/launches';
 
 // can be used in any components
@@ -9,13 +10,21 @@ import { Launches } from '../Model/launches';
 })
 export class AppService {
 
-  private  baseURL = "https://api.spacexdata.com/v4/launches";
+  private baseURL = "https://api.spacexdata.com/v4/launches";
   constructor(private httpClient: HttpClient) { }
 
 
   //return observable type any
- getAllLaunches():Observable<Launches[]> {
-   console.log("ça fonctionne dans le service : app.service.ts");
-   return this.httpClient.get<Launches[]>(`${this.baseURL}`);
-}
+  getAllLaunches(): Observable<Launches[]> {
+    console.log("ça fonctionne dans le service : app.service.ts");
+    return this.httpClient.get<Launches[]>(`${this.baseURL}`);
+  }
+  // get all launches from a rocket id
+  getLaunchesByRocketId(id:String): Observable<any> {
+    return this.httpClient.post<any>("https://api.spacexdata.com/v4/launches/query",{
+      query: {
+        rocket: id
+      }
+    }).pipe(map(res=>res.docs));
+  }
 }
