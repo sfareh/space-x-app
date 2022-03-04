676bdb41499580fdc3d0b6393342c383701a4606
RichardMargin <richard.mangin@mail.com>
2022-03-04 19:41:15 +0100

    added method that will call the service method for the launches by rocket

--- src/app/rockets/rockets.component.ts
+++ src/app/rockets/rockets.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {Rocket} from "../Model/rocket";
+import { Rocket } from "../Model/rocket";
+import { Launches } from "../Model/launches";
 import { RocketsService } from '../Services/rockets.service';
+import { AppService } from '../Services/app.service';
 
 @Component({
   selector: 'app-rockets',
@@ -10,14 +12,25 @@ import { RocketsService } from '../Services/rockets.service';
 export class RocketsComponent implements OnInit {
 
   rockets: Rocket[];
-  constructor(private rocketsService: RocketsService) { }
+  constructor(private rocketsService: RocketsService, private appService: AppService) { }
 
   ngOnInit(): void {
-    this.loadRockets();
+    this.rockets = [];
+    this.loadLaunchesRocket();
   }
 
   // Get rockets list
   private loadRockets() {
     return this.rocketsService.getRockets().subscribe(data => this.rockets = data);
+  }
+  // Get launches associated with rocket
+  private loadLaunchesRocket() {
+    return this.rocketsService.getRockets().subscribe(rocketsList => {
+      // pass Array of Rockets Id to get launches associated
+      rocketsList.map(rocket => this.appService.getLaunchesByRocketId(rocket.id).subscribe(launch => {
+        rocket.launches = launch
+        this.rockets.push(rocket);
+      }));
+    });
   }
 }
